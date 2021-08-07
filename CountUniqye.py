5cfe8122e89c13922ba2567121d355f422bc739e
Dhivesh Akilan <65279147+Dhivesh18@users.noreply.github.com>
2021-08-07 13:51:45 +0530

    Add files via upload

--- CountUniqye.py
+++ CountUniqye.py
@@ -1,17 +1,15 @@
 def fun(i):    
     t=[]
-    maxi,temp=0,0
+    temp=0
     while i!=0:
         r=i%10
         i//=10
         t.append(r)
     for i in t:
-        maxi=t.count(i)
-        if maxi>=temp:
-            temp=maxi
+        temp = t.count(i) if t.count(i)>=temp else temp
     return temp
 
-m,n,c=100,125,0
+m,n,c=1,100,0
 a=[i for i in range(m,n+1)]
 for i in a:
     x=fun(i)
