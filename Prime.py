d77f84caec3109bf8e7a47591bd68c27383ddc95
Dhivesh Akilan <65279147+Dhivesh18@users.noreply.github.com>
2021-09-12 19:30:05 +0530

    Prime Number Series Generator Added

--- Prime.py
+++ Prime.py
@@ -1,6 +1,12 @@
+#Check Whether NUmber is prime or not
 n=1
 x=[i for i in range(1,n) if n%i == 0]
 if len(x) == 1:
     print("Prime number")
 else:
     print("Not a prime number")
+
+# Prime Number series generator
+n=10
+k=[j for j in range(1,n+1) if len([i for i in range(1,j) if j%i == 0]) == 1]
+print(k)
